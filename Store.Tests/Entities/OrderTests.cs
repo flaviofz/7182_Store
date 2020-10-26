f7b99c1d171b18844ebd671c7e29f68415bd154e
flaviofz <flaviofz@gmail.com>
2020-10-26 20:32:14 -0300

    Segundo teste

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -8,13 +8,16 @@ namespace Store.Tests.Entities
     [TestClass]
     public class OrderTests
     {
+        private readonly Customer _customer = new Customer("Flavio Zavarise", "flaviofz@gmail.com");
+        private readonly Product _product = new Product("Produto 1", 10, true);
+        private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
         {
+            var order = new Order(_customer, 0, null);
 
-            var customer = new Customer("Flavio Zavarise", "flaviofz@gmail.com");
-            var order = new Order(customer, 0, null);
             Assert.AreEqual(8, order.Number.Length);
         }
 
@@ -22,7 +25,9 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
         {
-            Assert.Fail();
+            var order = new Order(_customer, 0, null);
+
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
         }
 
         [TestMethod]
